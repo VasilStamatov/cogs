9712ed3512edda2f3d1fc97e0e17e89a91c3273d
VasilStamatov <vasil_0000@abv.bg>
2017-02-06 14:30:38 +0000

    FIxed the camera rotation by mouse motion, fluidly locking the z axis now

--- cogs/FPSCameraControl.cpp
+++ cogs/FPSCameraControl.cpp
@@ -20,18 +20,32 @@ namespace cogs
 				}
 				void FPSCameraControl::update(float _deltaTime)
 				{
+						const glm::vec3& orientation = m_transform->worldOrientation();
+
+						//keep the z axis locked
+						m_transform->setWorldOrientation(glm::vec3(orientation.x, orientation.y, 0.0f));
+
+						// Handle movement
+
+						// Forward
 						if (utils::Input::isKeyDown(utils::KeyCode::W))
 						{
 								m_transform->translate(m_transform->localForwardAxis() * m_moveSpeed * _deltaTime);
 						}
+
+						// Backwards
 						if (utils::Input::isKeyDown(utils::KeyCode::S))
 						{
 								m_transform->translate(m_transform->localForwardAxis() * -m_moveSpeed * _deltaTime);
 						}
+
+						// Left
 						if (utils::Input::isKeyDown(utils::KeyCode::A))
 						{
 								m_transform->translate(m_transform->localRightAxis() * -m_moveSpeed * _deltaTime);
 						}
+
+						// Right
 						if (utils::Input::isKeyDown(utils::KeyCode::D))
 						{
 								m_transform->translate(m_transform->localRightAxis() * m_moveSpeed * _deltaTime);
@@ -39,6 +53,7 @@ namespace cogs
 
 						const glm::vec2& mouseMotion = utils::Input::getMouseMotion();
 
+						// Handle rotation
 						if (mouseMotion.x != 0.0f || mouseMotion.y != 0.0f)
 						{
 								//how much the mouse moves horizontally
@@ -47,11 +62,6 @@ namespace cogs
 								float yRotation = (-mouseMotion.x * m_moveSpeed * _deltaTime);
 
 								m_transform->rotate(glm::vec3(glm::radians(xRotation), glm::radians(yRotation), 0.0f));
-
-								const glm::vec3& orientation = m_transform->worldOrientation();
-
-								//keep the z axis locked
-								m_transform->setWorldOrientation(glm::vec3(orientation.x, orientation.y, 0.0f));
 
 								utils::Input::setMouseMotion(0.0f, 0.0f);
 						}
