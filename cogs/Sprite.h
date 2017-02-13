77597026eae6c7bfd41926b68985bfbf7860724e
VasilStamatov <vasil_0000@abv.bg>
2017-02-13 15:26:15 +0000

    The sprite is now not a component, but a graphics resource which is used in the new spriterenderer component

--- cogs/Sprite.h
+++ cogs/Sprite.h
@@ -1,76 +1,44 @@
 #ifndef SPRITE_H
 #define SPRITE_H
 
-#include "Component.h"
-#include "SpriteRenderer.h"
-#include "GLTexture.h"
+#include <glm\vec2.hpp>
+#include <memory>
+
+#include "Color.h"
+#include "GLTexture2D.h"
 
 namespace cogs
 {
-		namespace ecs
+		namespace graphics
 		{
-				class Transform;
 				/**
-						* The sprite component
+						* The sprite
 						*/
-				class Sprite : public Component
+				class Sprite
 				{
 				public:
-						/**
-								* Default sprite constructor
-								*/
-						Sprite();
+						Sprite(const std::string& _name, std::weak_ptr<GLTexture2D> _texture, const glm::vec2& _size, const Color& _color)
+								: m_name(_name), m_size(_size), m_color(_color), m_texture(_texture)
+						{
+						}
+						Sprite() {}
+						~Sprite() {}
 
-						/**
-								* Sprite contstructor
-								* \param _color - the color of the sprite
-								* \param _renderer - the renderer this sprite will submit itself to
-								*/
-						Sprite(const glm::vec2& _size, const graphics::Color& _color,
-								const std::string& _texturePath, bool _alpha, std::weak_ptr<graphics::SpriteRenderer> _renderer);
+						void setName(const std::string& _name) { m_name = _name; }
+						void setSize(const glm::vec2& _size) { m_size = _size; }
+						void setColor(const Color& _color) { m_color = _color; }
+						void setTexture(std::weak_ptr<GLTexture2D> _filePath) { m_texture = m_texture; }
 
-						/**
-								* Sprite destructor
-								*/
-						~Sprite();
-
-						/**
-								* The initialize component function
-								*/
-						void init() override;
-
-						/**
-								* The update component function
-								*/
-						void update(float _deltaTime) override;
-
-						/**
-						 	* The render component function
-						 	*/
-						void render(std::weak_ptr<Camera> _camera) override;
-
-						void setTexture(const std::string& _filePath, bool _alpha);
-						void setRenderer(std::weak_ptr<graphics::SpriteRenderer> _renderer);
-
-						/**
-								* Getters
-								*/
-						inline const graphics::Color& getColor()								const noexcept { return m_color; }
-						inline const glm::vec2& getSize()														 const noexcept { return m_size; }
-						inline const std::array<glm::vec2, 4>& getUV()		const noexcept	{ return m_uv; }
-						inline const graphics::GLTexture& getTexture()  const noexcept { return m_texture; }
+						const std::string& getName() { return m_name; }
+						const glm::vec2& getSize() { return m_size; }
+						const Color& getColor() { return m_color; }
+				  std::weak_ptr<GLTexture2D> getTexture() { return m_texture; }
 
 				private:
-						void setUVdefaults();
-
-				private:
-						std::weak_ptr<Transform> m_transform;
-						graphics::Color m_color;
-						glm::vec2 m_size{ 0.0f };
-						std::array<glm::vec2, 4> m_uv;
-
-						graphics::GLTexture m_texture;
-						std::weak_ptr<graphics::SpriteRenderer> m_renderer;
+						std::string m_name{ "" };
+						glm::vec2 m_size{ 0.0f, 0.0f };
+						Color m_color{ Color::white };
+						std::weak_ptr<GLTexture2D> m_texture;
 				};
 		}
 }
