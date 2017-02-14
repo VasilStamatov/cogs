49dd41d937cafeaaef904c2b90d3c149d6fef683
VasilStamatov <vasil_0000@abv.bg>
2017-02-14 09:34:14 +0000

    Added a skybox example in main.cpp

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -18,7 +18,7 @@
 #include "PaddleController.h"
 #include "BallBehavior.h"
 
-#define DEBUG_DRAW 1
+#define DEBUG_DRAW 0
 
 namespace ce = cogs::ecs;
 namespace cg = cogs::graphics;
@@ -38,17 +38,30 @@ int main(int argc, char** argv)
 
 		std::shared_ptr<ce::Entity> root = ce::Entity::create("Root");
 
-		std::shared_ptr<cg::Framebuffer> test = cg::Framebuffer::create(window.getWidth(), window.getHeight());
+		//std::shared_ptr<cg::Framebuffer> testFramebuffer = cg::Framebuffer::create(window.getWidth(), window.getHeight());
+
+		std::vector<std::string> skyboxFilenames(6);
+		skyboxFilenames.at(0) = "Textures/skybox/right.png";
+		skyboxFilenames.at(1) = "Textures/skybox/left.png";
+		skyboxFilenames.at(2) = "Textures/skybox/top.png";
+		skyboxFilenames.at(3) = "Textures/skybox/bottom.png";
+		skyboxFilenames.at(4) = "Textures/skybox/back.png";
+		skyboxFilenames.at(5) = "Textures/skybox/front.png";
+
+		std::shared_ptr<cg::Skybox> testSkybox = cg::Skybox::create(
+				cu::ResourceManager::getGLSLProgram("SkyboxShader", "Shaders/Skybox.vert", "Shaders/Skybox.frag"),
+				cu::ResourceManager::getGLCubemap("skyboxTexture", skyboxFilenames), true);
 
 		std::weak_ptr<ce::Entity> mainCamera = root->addChild("MainCamera");
 		mainCamera.lock()->addComponent<ce::Camera>(window.getWidth(), window.getHeight(), ce::ProjectionType::PERSPECTIVE);
 		mainCamera.lock()->addComponent<ce::FPSCameraControl>(1.0f);
 		mainCamera.lock()->getComponent<ce::Transform>().lock()->translate(glm::vec3(0.0f, 0.0f, 55.0f));
+		mainCamera.lock()->getComponent<ce::Camera>().lock()->setSkybox(testSkybox);
 		//mainCamera.lock()->getComponent<ce::Camera>().lock()->setRenderTarget(test);
 
 		/*std::weak_ptr<ce::Entity> camera2 = root->addChild("Camera2");
-		camera2.lock()->addComponent<ce::Camera>(ce::ProjectionType::PERSPECTIVE, window.getWidth(), window.getHeight());
-		camera2.lock()->getComponent<ce::Transform>()->translate(glm::vec3(0.0f, 0.0f, 25.0f));*/
+		camera2.lock()->addComponent<ce::Camera>(window.getWidth(), window.getHeight(), ce::ProjectionType::PERSPECTIVE);
+		camera2.lock()->getComponent<ce::Transform>().lock()->translate(glm::vec3(0.0f, 0.0f, 25.0f));*/
 
 		std::shared_ptr<cg::Renderer2D> renderer2D = std::make_shared<cg::Renderer2D>(
 				cu::ResourceManager::getGLSLProgram("BasicShader", "Shaders/BasicShader.vert", "Shaders/BasicShader.frag"));
@@ -56,12 +69,12 @@ int main(int argc, char** argv)
 		std::shared_ptr<cg::Renderer3D> renderer3D = std::make_shared<cg::Renderer3D>(
 				cu::ResourceManager::getGLSLProgram("Basic3DShader", "Shaders/Basic3DShader.vert", "Shaders/Basic3DShader.frag"));
 
-		std::weak_ptr<ce::Entity> testSprite = root->addChild("testSprite");
+		/*std::weak_ptr<ce::Entity> testSprite = root->addChild("testSprite");
 		testSprite.lock()->addComponent<ce::SpriteRenderer>(
 				cu::ResourceManager::getSprite("TestSprite",
 						cu::ResourceManager::getGLTexture2D("Textures/img_test.png","texture_diffuse"),
 						glm::vec2(10.0f, 10.0f), cg::Color::white),
-				cu::ResourceManager::getMaterial("TestMaterial"), renderer2D);
+				cu::ResourceManager::getMaterial("TestMaterial"), renderer2D);*/
 
 		/*std::weak_ptr<ce::Entity> nanosuit = root->addChild("nanosuit");
 		nanosuit.lock()->addComponent<ce::MeshRenderer>(std::make_shared<cg::Model>("nanosuit", "Models/nanosuit/nanosuit.obj"),
@@ -249,6 +262,9 @@ int main(int argc, char** argv)
 						debugRenderer.end();
 						debugRenderer.render(camera.lock()->getViewMatrix(), camera.lock()->getProjectionMatrix(), 5.0f);
 #endif //  DEBUG_DRAW
+
+						//render the camera's skybox if it has one
+						camera.lock()->renderSkybox();
 
 						//set the render target to the default window
 						cg::Framebuffer::setActive(std::weak_ptr<cg::Framebuffer>());
