03e50b864249ccc63a1f542d21da9a804f8f27d5
VasilStamatov <vasil_0000@abv.bg>
2017-02-14 09:06:57 +0000

    Added the extern function loadCubemap to load an opengl cubemap to utils

--- cogs/Utils.cpp
+++ cogs/Utils.cpp
@@ -57,6 +57,35 @@ namespace cogs
 						return true;
 				}
 
+				bool loadCubemap(const char ** _fileNames, int * _width, int * _height, unsigned int * _id)
+				{
+						//Generate the openGL handle for the cubemap
+						glGenTextures(1, _id);
+
+						//bind the cubemap
+						glBindTexture(GL_TEXTURE_CUBE_MAP, *_id);
+
+						for (GLuint i = 0; i < 6; i++)
+						{
+								unsigned char* image = SOIL_load_image(_fileNames[i], _width, _height, 0, SOIL_LOAD_RGB);
+								if (image == nullptr)
+								{
+										return false;
+								}
+								glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, *_width, *_height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+								SOIL_free_image_data(image);
+						}
+
+						glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+						glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+						glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+						glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+						glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+						glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
+						return true;
+				}
+
 				std::vector<graphics::Mesh> loadModel(const std::string & _filePath)
 				{
 						Assimp::Importer importer;
--- cogs/Utils.h
+++ cogs/Utils.h
@@ -28,6 +28,22 @@ namespace cogs
 				extern bool loadTexture(const char* _filePath, bool _alpha, int* _width, int* _height, unsigned int* _id);
 
 				/**
+				* \brief Loads a texture using SOIL, returns true if successful.
+				* \param _fileNames- the 6 filepaths of the cubemap (6 textures) order should be:
+				*	+X (right)
+				*	-X (left)
+				*	+Y (top)
+				*	-Y (bottom)
+				*	-Z (back)
+				*	+Z (front)
+				* \param _alpha			 - flag whether the texture has alpha or not
+				* \param _width			 - address of an integer to write to for the texture's width
+				* \param _height		 - address of an integer to write to for the texture's height
+				* \param _id						 - address of an integer to write to for the texture's opengl texture ID
+				*/
+				extern bool loadCubemap(const char** _fileNames, int* _width, int* _height, unsigned int* _id);
+
+				/**
 				* \brief Loads a model from a file using ASSIMP, 
 				* \param _filePath - the filepath of the model
 				* \return a vector of all the meshes the model is constructed from
