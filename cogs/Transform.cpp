a2fbea0be39eb510751ad517773f8277783c62f0
VasilStamatov <vasil_0000@abv.bg>
2017-02-18 10:22:53 +0000

    Restructucred the transform class, it now only contains local space data and calculates world space data when requested by function calls

--- cogs/Transform.cpp
+++ cogs/Transform.cpp
@@ -2,18 +2,18 @@
 #include "Entity.h"
 
 #include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtx\matrix_decompose.hpp>
 
 namespace cogs
 {
 		namespace ecs
 		{
-				Transform::Transform(const glm::vec3 & _pos /* = glm::vec3(0.0f)  */,
-						const glm::vec3 & _eulerAngles 											/* = identity quat				*/,
-						const glm::vec3 & _scale 																	/* = glm::vec3 (1.0f) */)
+				Transform::Transform(const glm::vec3 & _pos,
+						const glm::vec3 & _eulerAngles, const glm::vec3 & _scale)
 				{
-						internal_setWorldPosition(_pos);
-						internal_setWorldOrientation(_eulerAngles);
-						internal_setWorldScale(_scale);
+						setLocalPosition(_pos);
+						setLocalScale(_scale);
+						setLocalOrientation(_eulerAngles);
 				}
 
 				Transform::~Transform()
@@ -29,47 +29,73 @@ namespace cogs
 				{
 						glm::quat toRotate(_eulerAngles);
 
-						internal_setLocalOrientation(toRotate * m_localOrientationRaw);
+						setLocalOrientation(toRotate * m_localOrientationRaw);
 				}
 
 				void Transform::rotate(const glm::vec3 & _axis, float _angle)
 				{
 						glm::quat toRotate(glm::angleAxis((_angle), _axis));
 
-						internal_setLocalOrientation(toRotate * m_localOrientationRaw);
+						setLocalOrientation(toRotate * m_localOrientationRaw);
 				}
 
 				void Transform::rotate(const glm::quat & _rotationQuat)
 				{
-						internal_setLocalOrientation(_rotationQuat * m_localOrientationRaw);
+						setLocalOrientation(_rotationQuat * m_localOrientationRaw);
 				}
 
 				void Transform::lookAt(const glm::vec3 & _target)
 				{
-						glm::vec3 direction = glm::normalize(_target - m_worldPosition);
-						glm::mat4 lookAtMat = glm::lookAt(m_worldPosition, m_worldPosition + direction, worldUpAxis());
-						internal_setLocalOrientation(glm::quat_cast(lookAtMat));
+						//TODO: Implement!
 				}
 
 				void Transform::translate(const glm::vec3 & _offset)
 				{
-						internal_setLocalPosition(m_localPosition + _offset);
+						m_localPosition += _offset;
 				}
 
-				void Transform::offsetScale(const glm::vec3 & _newScale)
+				void Transform::offsetScale(const glm::vec3 & _offset)
 				{
-						internal_setLocalScale(m_localScale + _newScale);
+						m_localScale += _offset;
 				}
 
 				glm::mat4 Transform::localTransform() const
 				{
 						glm::mat4 translation = glm::translate(glm::mat4(1.0f), m_localPosition);
-						glm::mat4 scale							= glm::scale(glm::mat4(1.0f), m_localScale);
-						glm::mat4 rotation				= glm::mat4_cast(m_localOrientationRaw);
+						glm::mat4 scale = glm::scale(glm::mat4(1.0f), m_localScale);
+						glm::mat4 rotation = glm::mat4_cast(m_localOrientationRaw);
 
 						glm::mat4 localTransform = translation * rotation * scale;
 
 						return localTransform;
+				}
+
+			 glm::vec3 Transform::worldPosition() const noexcept
+				{
+						glm::mat4 worldTrs = worldTransform();
+
+						return glm::vec3(worldTrs[3]);
+				}
+
+			 glm::vec3 Transform::worldOrientation() const noexcept
+				{
+						glm::mat4 worldTrs = worldTransform();
+
+						return glm::eulerAngles(glm::quat(worldTrs));
+				}
+
+			 glm::quat Transform::worldOrientationRaw() const noexcept
+				{
+						glm::mat4 worldTrs = worldTransform();
+
+						return glm::quat(worldTrs);
+				}
+
+			 glm::vec3 Transform::worldScale() const noexcept
+				{
+						glm::mat4 worldTrs = worldTransform();
+
+						return glm::vec3(worldTrs[0][0], worldTrs[1][1], worldTrs[2][2]);
 				}
 
 				glm::mat4 Transform::worldTransform() const
@@ -84,17 +110,122 @@ namespace cogs
 						return worldTrans;
 				}
 
+				inline void Transform::setLocalOrientation(const glm::quat & _value)
+				{
+						//set the raw orientation value
+						m_localOrientationRaw = glm::normalize(_value);
+
+						//set the orientation's euler angles representation
+						m_localOrientation = glm::eulerAngles(m_localOrientationRaw);
+				}
+
+				void Transform::setLocalOrientation(const glm::vec3 & _value)
+				{
+						setLocalOrientation(glm::quat(_value));
+				}
+
+				void Transform::setLocalPosition(const glm::vec3 & _value)
+				{
+						//set the new world position
+						m_localPosition = _value;
+				}
+
+				void Transform::setLocalScale(const glm::vec3 & _value)
+				{
+						//set the new world position
+						m_localScale = _value;
+				}
+
+				void Transform::setWorldOrientation(const glm::quat & _value)
+				{
+						// set the raw orientation value
+						glm::quat worldOrient = glm::normalize(_value);
+
+						//update the local orientation to be correct
+						if (!m_parent.expired())
+						{
+								//to get the correct local orientation, multiply the child's world rotation by the parent's conjugate
+								m_localOrientationRaw = worldOrient * glm::normalize(glm::conjugate(m_parent.lock()->worldOrientationRaw()));
+						}
+						else
+						{
+								//if there is no parent, world == local
+								m_localOrientationRaw = worldOrient;
+						}
+
+						//set the local orientation's euler angle representation
+						m_localOrientation = glm::eulerAngles(m_localOrientationRaw);
+				}
+
+				void Transform::setWorldOrientation(const glm::vec3 & _value)
+				{
+						setWorldOrientation(glm::quat(_value));
+				}
+
+				void Transform::setWorldPosition(const glm::vec3 & _value)
+				{
+						//update the local position to be correct
+						if (!m_parent.expired())
+						{
+								//to get the correct local position, subtract the parent's world translation from the child's world translation
+								m_localPosition = _value - m_parent.lock()->worldPosition();
+						}
+						else
+						{
+								//if there is no parent, world == local
+								m_localPosition = _value;
+						}
+				}
+
+				void Transform::setWorldScale(const glm::vec3 & _value)
+				{
+						//update the local scale to be correct
+						if (!m_parent.expired())
+						{
+								//to get the correct local scale, multiply the child's world scale with the reciprocal of the parent's world scale
+								m_localScale = _value * (1.0f / m_parent.lock()->worldScale());
+						}
+						else
+						{
+								//if there is no parent, world == local
+								m_localScale = _value;
+						}
+				}
+
+				void Transform::setParent(std::weak_ptr<Transform> _parent)
+				{
+						if (_parent.expired())
+						{
+								setLocalPosition(worldPosition());
+								setLocalScale(worldScale());
+								setLocalOrientation(worldOrientationRaw());
+								m_parent.reset();
+						}
+						else
+						{
+								setLocalPosition(worldPosition());
+								setLocalScale(worldScale());
+								setLocalOrientation(worldOrientationRaw());
+
+								m_parent = _parent;
+
+								setWorldPosition(m_localPosition);
+								setWorldScale(m_localScale);
+								setWorldOrientation(m_localOrientationRaw);
+						}
+				}
+
 				bool Transform::operator==(const Transform & _rhs) const
 				{
-						auto boolVec = glm::equal(m_worldPosition, _rhs.m_worldPosition);
+						auto boolVec = glm::equal(worldPosition(), _rhs.worldPosition());
 
 						if ((boolVec.x == true) && (boolVec.y == true) && (boolVec.z == true))
 						{
-								boolVec = glm::equal(m_worldScale, _rhs.m_worldScale);
+								boolVec = glm::equal(worldScale(), _rhs.worldScale());
 
 								if ((boolVec.x == true) && (boolVec.y == true) && (boolVec.z == true))
 								{
-										boolVec = glm::equal(m_worldOrientation, _rhs.m_worldOrientation);
+										boolVec = glm::equal(worldOrientation(), _rhs.worldOrientation());
 
 										if ((boolVec.x == true) && (boolVec.y == true) && (boolVec.z == true))
 										{
@@ -103,137 +234,6 @@ namespace cogs
 								}
 						}
 						return false;
-				}
-
-				void Transform::internal_setLocalOrientation(const glm::quat & _value)
-				{
-						//set the raw orientation value
-						m_localOrientationRaw = glm::normalize(_value);
-
-						//set the orientation's euler angles representation
-						m_localOrientation = glm::eulerAngles(m_localOrientationRaw);
-
-						//update the world orientation to be correct
-						if (!m_parent.expired())
-						{
-								//to get the correct world orientation, multiply parent's world orientation by the childs local orientation
-								m_worldOrientationRaw = glm::normalize(m_parent.lock()->m_worldOrientationRaw * m_localOrientationRaw);
-						}
-						else
-						{
-								//if there is no parent, world == local
-								m_worldOrientationRaw = m_localOrientationRaw;
-						}
-
-						//set the world orientation's euler angle representation
-						m_worldOrientation = glm::eulerAngles(m_worldOrientationRaw);
-				}
-
-				void Transform::internal_setLocalOrientation(const glm::vec3 & _value)
-				{
-						//convert the euler angle value to a quat and pass it to the overloaded function
-						internal_setLocalOrientation(glm::quat(_value));
-				}
-
-				void Transform::internal_setLocalPosition(const glm::vec3 & _value)
-				{
-						//set the new world position
-						m_localPosition = _value;
-
-						//update the world position to be correct
-						if (!m_parent.expired())
-						{
-								//to get the correct world position, simply add the parent's world translation from the child's local translation
-								m_worldPosition = m_parent.lock()->m_worldPosition + m_localPosition;
-						}
-						else
-						{
-								//if there is no parent, world == local
-								m_worldPosition = m_localPosition;
-						}
-				}
-
-				void Transform::internal_setLocalScale(const glm::vec3 & _value)
-				{
-						//set the new local scale
-						m_localScale = _value;
-
-						//update the world scale to be correct
-						if (!m_parent.expired())
-						{
-								//to get the correct world scale, multiply the components of the parent against the child's
-								m_worldScale = m_parent.lock()->m_worldScale * m_localScale;
-						}
-						else
-						{
-								//if there is no parent, world == local
-								m_worldScale = m_localScale;
-						}
-				}
-
-				void Transform::internal_setWorldOrientation(const glm::quat & _value)
-				{
-						// set the raw orientation value
-						m_worldOrientationRaw = glm::normalize(_value);
-
-						//set the orientation's euler angles representation
-						m_worldOrientation = glm::eulerAngles(m_worldOrientationRaw);
-
-						//update the local orientation to be correct
-						if (!m_parent.expired())
-						{
-								//to get the correct local orientation, multiply the child's world rotation by the parent's conjugate
-								m_localOrientationRaw = m_worldOrientationRaw * glm::normalize(glm::conjugate(m_parent.lock()->m_worldOrientationRaw));
-						}
-						else
-						{
-								//if there is no parent, world == local
-								m_localOrientationRaw = m_worldOrientationRaw;
-						}
-
-						//set the local orientation's euler angle representation
-						m_localOrientation = glm::eulerAngles(m_localOrientationRaw);
-				}
-
-				void Transform::internal_setWorldOrientation(const glm::vec3 & _value)
-				{
-						//convert the euler angle value to a quat and pass it to the overloaded function
-						internal_setWorldOrientation(glm::quat(_value));
-				}
-
-				void Transform::internal_setWorldPosition(const glm::vec3 & _value)
-				{
-						//set the new world position
-						m_worldPosition = _value;
-
-						//update the local position to be correct
-						if (!m_parent.expired())
-						{
-								//to get the correct local position, subtract the parent's world translation from the child's world translation
-								m_localPosition = m_worldPosition - m_parent.lock()->m_worldPosition;
-						}
-						else
-						{
-								//if there is no parent, world == local
-								m_localPosition = m_worldPosition;
-						}
-				}
-				void Transform::internal_setWorldScale(const glm::vec3 & _value)
-				{
-						//set the new local scale
-						m_worldScale = _value;
-
-						//update the local scale to be correct
-						if (!m_parent.expired())
-						{
-								//to get the correct local scale, multiply the child's world scale with the reciprocal of the parent's world scale
-								m_localScale = m_worldScale * (1.0f / m_parent.lock()->m_worldScale);
-						}
-						else
-						{
-								//if there is no parent, world == local
-								m_localScale = m_worldScale;
-						}
 				}
 		}
 }
--- cogs/Transform.h
+++ cogs/Transform.h
@@ -12,24 +12,25 @@ namespace cogs
 				class Transform : public Component
 				{
 				public:
+						Transform() {}
 						/**
-								*	\brief default contructor to contain the transform of an entity in the world
-								* \param _pos - the world vec3 coordinates (set to origin (0,0,0) by default
-								* \param _eulerAngles - the orientation on each axis in radians (set to 0 on each axis by default)
-								* \param _scale - the scale on each axis (set to 1 by default)
-								*/
-						Transform(const glm::vec3& _pos = glm::vec3(0.0f),
-								const glm::vec3& _eulerAngles = glm::vec3(0.0f, 0.0f, 0.0f),
-								const glm::vec3& _scale = glm::vec3(1.0f));
+						*	\brief Constructs the transform using positon, orientation and scale
+						* \param _pos - the world vec3 coordinates (set to origin (0,0,0) by default
+						* \param _eulerAngles - the orientation on each axis in radians (set to 0 on each axis by default)
+						* \param _scale - the scale on each axis (set to 1 by default)
+						*/
+						Transform(const glm::vec3& _pos,
+								const glm::vec3& _eulerAngles,
+								const glm::vec3& _scale);
 						~Transform();
 
 						void init() override;
 						void update(float _deltaTime) override;
 
 						/**
-						 	*	\brief rotates the transform of the entity by euler angles in radians
-						 	* \param _eulerAngles - vec3 euler angles in radians to be added
-						 	*/
+				 	*	\brief rotates the transform of the entity by euler angles in radians
+				 	* \param _eulerAngles - vec3 euler angles in radians to be added
+				 	*/
 						void rotate(const glm::vec3& _eulerAngles);
 						
 						/**
@@ -45,110 +46,131 @@ namespace cogs
 						void rotate(const glm::quat& _rotationQuat);
 
 						/**
-						  *	\brief sets the orientation of the transform to be looking at the set target
-						  * \param _target - the vec3 target the orientation should be looking at
-						  */
+					 *	\brief sets the orientation of the transform to be looking at the set target
+					 * \param _target - the vec3 target the orientation should be looking at
+					 */
 						void lookAt(const glm::vec3& _target);
 
 						/**
-						  *	\brief translates the position of the transform by _offset
-						  * \param _offset - the vec3 offset to add
-						  */
+						*	\brief translates the position of the transform by _offset
+						* \param _offset - the vec3 offset to add
+						*/
 						void translate(const glm::vec3& _offset);
 
 						/**
-						  *	\brief scales the local scale of the transform by _offset
-						  * \param _offset - the vec3 offset to add
-						  */
-						void offsetScale(const glm::vec3& _newScale);
+						*	\brief scales the local scale of the transform by _offset
+						* \param _offset - the vec3 offset to add
+						*/
+						void offsetScale(const glm::vec3& _offset);
 
 						/**
-								*	\brief local space getters
-								*/
-						inline const glm::vec3& localPosition()			 const noexcept { return m_localPosition; }
+						*	\brief returns the local space position
+						*/
+						inline const glm::vec3& localPosition()	const noexcept { return m_localPosition; }
+
+						/**
+						*	\brief returns the local space orientation in euler angles (radians)
+						*/
 						inline const glm::vec3& localOrientation() const noexcept { return m_localOrientation; }
-						inline const glm::vec3& localScale()						 const noexcept { return m_localScale; }
-
-						glm::mat4 localTransform()		 const;
 
 						/**
-								*	\brief world space getters
-								*/
-						inline const glm::vec3& worldPosition()			 const noexcept { return m_worldPosition; }
-						inline const glm::vec3& worldOrientation() const noexcept { return m_worldOrientation; }
-						inline const glm::vec3& worldScale()							const noexcept { return m_worldScale; }
-
-						glm::mat4 worldTransform()		 const;
+						*	\brief returns the local space scale
+						*/
+						inline const glm::vec3& localScale() const noexcept { return m_localScale; }
 
 						/**
-								*	\brief Getters for the local axis of the transform
-								* \return the direction vec3
-								*/
-						glm::vec3 localRightAxis()		 const noexcept { return m_localOrientationRaw * glm::vec3(1.0f, 0.0f, 0.0f); }
-						glm::vec3 localUpAxis()					 const noexcept { return m_localOrientationRaw * glm::vec3(0.0f, 1.0f, 0.0f); }
+						*	\brief returns the local space model matrix
+						*/
+						glm::mat4 localTransform()	const;
+
+						/**
+						*	\brief returns the world space position
+						*/
+					 glm::vec3 worldPosition() const noexcept;
+
+						/**
+						*	\brief returns the world space orientation in euler angles (radians)
+						*/
+					 glm::vec3 worldOrientation() const noexcept;
+
+						/**
+						*	\brief returns the world space orientation as a quat
+						*/
+					 glm::quat worldOrientationRaw() const noexcept;
+
+						/**
+						*	\brief returns the world space scale
+						*/
+					 glm::vec3 worldScale()	const noexcept;
+
+						/**
+						*	\brief returns the world space model matrix
+						*/
+						glm::mat4 worldTransform() const;
+
+						/**
+						*	\brief Returns the right axis using the local space coordinates (direction to the right (positive x axis))
+						*/
+						glm::vec3 localRightAxis() const noexcept { return m_localOrientationRaw * glm::vec3(1.0f, 0.0f, 0.0f); }
+
+						/**
+						*	\brief Returns the up axis using the local space coordinates (direction pointing up (positive y axis))
+						*/
+						glm::vec3 localUpAxis()	const noexcept { return m_localOrientationRaw * glm::vec3(0.0f, 1.0f, 0.0f); }
+
+						/**
+						*	\brief Returns the forward axis using the local space coordinates
+						* (direction pointing forwards (to the negative z axis as opengl is using right handed coordinate system))
+						*/
 						glm::vec3 localForwardAxis() const noexcept { return m_localOrientationRaw * glm::vec3(0.0f, 0.0f, -1.0f); }
 
 						/**
-								*	\brief Getters for the world axis of the transform
-								* \return the direction vec3
-								*/
-						glm::vec3 worldRightAxis()		 const noexcept { return m_worldOrientationRaw * glm::vec3(1.0f, 0.0f, 0.0f); }
-						glm::vec3 worldUpAxis()					 const noexcept { return m_worldOrientationRaw * glm::vec3(0.0f, 1.0f, 0.0f); }
-						glm::vec3 worldForwardAxis() const noexcept { return m_worldOrientationRaw * glm::vec3(0.0f, 0.0f, -1.0f); }
+						*	\brief Returns the right axis using the world space coordinates (direction to the right (positive x axis))
+						*/
+						glm::vec3 worldRightAxis() const noexcept { return worldOrientationRaw() * glm::vec3(1.0f, 0.0f, 0.0f); }
+
+						/**
+						*	\brief Returns the up axis using the world space coordinates (direction pointing up (positive y axis))
+						*/
+						glm::vec3 worldUpAxis()	const noexcept { return worldOrientationRaw() * glm::vec3(0.0f, 1.0f, 0.0f); }
+
+						/**
+						*	\brief Returns the forward axis using the world space coordinates
+						* (direction pointing forwards (to the negative z axis as opengl is using right handed coordinate system))
+						*/
+						glm::vec3 worldForwardAxis() const noexcept { return worldOrientationRaw() * glm::vec3(0.0f, 0.0f, -1.0f); }
 
 						/**
 								*	\brief setters for local transform
 								*/
-						inline void setLocalOrientation(const glm::quat& _value) { internal_setLocalOrientation(_value); }
-						inline void setLocalOrientation(const glm::vec3& _value) { internal_setLocalOrientation(_value); }
-						inline void setLocalPosition		 (const glm::vec3& _value)	{ internal_setLocalPosition		 (_value); }
-						inline void setLocalScale					 (const glm::vec3& _value)	{ internal_setLocalScale					 (_value); }
+						void setLocalOrientation(const glm::quat& _value);
+						void setLocalOrientation(const glm::vec3& _value);
+						void setLocalPosition		 (const glm::vec3& _value);
+						void setLocalScale					 (const glm::vec3& _value);
 
 						/**
 								*	\brief setters for world transform
 								*/
-						inline void setWorldOrientation(const glm::quat& _value) { internal_setWorldOrientation(_value); }
-						inline void setWorldOrientation(const glm::vec3& _value) { internal_setWorldOrientation(_value); }
-						inline void setWorldPosition		 (const glm::vec3& _value)	{ internal_setWorldPosition		 (_value); }
-						inline void setWorldScale					 (const glm::vec3& _value)	{ internal_setWorldScale					 (_value); }
+						void setWorldOrientation(const glm::quat& _value);
+						void setWorldOrientation(const glm::vec3& _value);
+						void setWorldPosition		 (const glm::vec3& _value);
+						void setWorldScale					 (const glm::vec3& _value);
 
 						/**
 						 	*	\brief Getter and setter for the parent
 						 	*/
-						inline void setParent(std::weak_ptr<Transform> _parent)					{ m_parent = _parent; }
+					 void setParent(std::weak_ptr<Transform> _parent);
 						inline std::weak_ptr<Transform> getParent()		const noexcept	{ return m_parent; }
-
-						inline std::weak_ptr<Entity> getHolder() { return m_entity; }
 
 						bool operator== (const Transform& _rhs) const;
 
 				private:
-						/**
-								* Internal functions for setting the transform attributes
-								*/
-						void internal_setLocalOrientation(const glm::quat& _value);
-						void internal_setLocalOrientation(const glm::vec3& _value);
-						void internal_setLocalPosition		 (const glm::vec3& _value);
-						void internal_setLocalScale					 (const glm::vec3& _value);
-																																																																				
-						void internal_setWorldOrientation(const glm::quat& _value);
-						void internal_setWorldOrientation(const glm::vec3& _value);
-						void internal_setWorldPosition		 (const glm::vec3& _value);
-						void internal_setWorldScale					 (const glm::vec3& _value);
-
-				private:
 						std::weak_ptr<Transform> m_parent;
 
-						//The transform is the result of scaling->rotating->translating
-						glm::vec3 m_worldPosition;
-						glm::vec3 m_worldScale;
-						glm::vec3 m_worldOrientation;
-						glm::quat m_worldOrientationRaw;
-
-						glm::vec3 m_localPosition;
-						glm::vec3 m_localScale;
-						glm::vec3 m_localOrientation;
-						glm::quat m_localOrientationRaw;
+						glm::vec3 m_localPosition{ 0.0f, 0.0f, 0.0f };
+						glm::vec3 m_localScale{ 1.0f, 1.0f, 1.0f };
+						glm::vec3 m_localOrientation{ 0.0f, 0.0f, 0.0f };
+						glm::quat m_localOrientationRaw{ glm::vec3(0.0f, 0.0f, 0.0f) };
 				};
 		}
 }
